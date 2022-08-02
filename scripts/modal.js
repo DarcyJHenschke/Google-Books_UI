58448dbbb9b3023734e2c826a610094f383ef8cd
Darcy Henschke <darcyhenschke@Darcys-MacBook-Air.local>
2022-08-02 14:52:12 +1000

    tidying up

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,8 +1,1 @@
-export let renderModal = `<div class='modal>
-        <div class='modal__content'>
-            <p class='modal-text modal__content--title>${title}</p>'
-            <p class='modal-text modal__content--authors>${authors}</p>'
-            <p class='modal-text modal__content--description>${description}</p>'
-            <p class='modal-text modal__content--ISBN>${ISBN}</p>'
-        </div>
-    </div>'`;
+
